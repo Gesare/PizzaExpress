85fdfce295d9d97b3b77bb948e18dab55b5e565f
Gesare <gesareayienda@gmail.com>
2020-03-02 11:54:24 +0300

    Changed css and abit of html

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,16 @@
 $("document").ready(function(){
-    function person (name,email,phone){
+    function person (name){
         this.name = name;
-        this.email = email;
-        this.phone = phone;
+
     }
         person.prototype.profile = function(){
          $("#name").text("Your name is "+this.name)
-         $("#email").text("Your Email address is "+this.email)
-         $("#phone").text("Your Phone number is "+this.phone)
+
         }
         $("#profile").click(function(){
             var name = $(".name").val();
-            var email =$(".Email").val();
-            var phone = $(".phone").val();
             var location = $(".location").val();
-             var user = new person(name,email,phone,location)
+             var user = new person(name,location)
             user.profile()
         })
     function order(size,toppings,number,delivery,type){
@@ -28,34 +24,34 @@ $("document").ready(function(){
         let price =0;
         let Total =0;
         if (this.size === "Large"){
-         price =1100;
+         price =1300;
          Total = price + 300;
          Total = Total * this.number;
         }
         else if (this.size === "Medium"){
-            price = 800;
+            price = 900;
             Total = price + 300;
             Total =Total * this.number;
         }
         else if(this.size ==="small"){
-            price=500
+            price=600
             Total = price + 300;
             Total = Total * this.number;
         }
         else alert("Enter your sizes ")
-        var delivery=prompt("Would like it hand delivered or you would like to proceed to our restaurant ?")
-        if(delivery === "hand delivered"){
-            alert("You'll have to add ksh.300 to your full price ")
-            Total = Total + 300;
+        var delivery=prompt("If you want your pizza to br delivered to your location.Answer yes.If not answer nope ?")
+        if(delivery === "yes"){
+            alert("You'll have to add ksh.150 to your full price ")
+            Total = Total + 150;
              var location =prompt("Please enter your location")
              $("#location").text("Your location is "+location)
         }
-        else if(delivery === "proceed to restaurant"){
-            alert("We are expecting you soon");
+        else if(delivery === "nope"){
+            alert("See you soon.");
         }
-        else alert('Wrong choice');
+        else alert('Please input the correct answer.');
 
-        $("#details").text("Your pizza will be ready shortly,The total price is  "+Total +"." +" And your  pizza size is  "+this.size);
+        $("#details").text("Your order will be delivered shortly.your total payment is "+Total +"." +" And you choose this type of pizza "+this.size);
 
     }
      $(".calc").click(function(){
@@ -66,6 +62,7 @@ $("document").ready(function(){
          var delivery = $(".delivery").val();
           var myOrder = new order(size,toppings,number,delivery,type);
           myOrder.price()
+
         });
         $(".A").hover(function(){
           $(".large").slideToggle()
